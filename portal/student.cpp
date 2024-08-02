a0f03a42644580f7da6c9c95d3df8838355f8eb8
alifbinturjo <107807077+alifbinturjo@users.noreply.github.com>
2024-08-02 14:58:18 +0600

    completed

--- portal/student.cpp
+++ portal/student.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<string>
 #include<stdlib.h>
+#include<fstream>
+#include<sstream>
 #include"headers.h"
 #include"student.h"
 using namespace std;
@@ -14,7 +16,8 @@ void Student::showMenu(){
     cout<<"Welcome "<<userName<<endl<<endl;
     cout<<"1. My details"<<endl;
     cout<<"2. Mails"<<endl;
-    cout<<"3. Logout"<<endl;
+    cout<<"3. Parent register"<<endl;
+    cout<<"4. Logout"<<endl;
 
     cout<<"Enter an option: ";
     cin>>opt;
@@ -30,6 +33,9 @@ void Student::menuWorker(){
         mails();
         break;
     case 3:
+        parentReg();
+        break;
+    case 4:
         performLogin();
         break;
     default:
@@ -37,8 +43,56 @@ void Student::menuWorker(){
     }
 }
 void Student::myDetail(){
+    system("CLS");
+    ifstream file("generaldata/allstudents.txt");
+    while(getline(file,line)){
+        stringstream ss(line);
+        ss>>getName>>name>>major>>cgpa;
 
+        if(getName==userName){
+            cout<<name<<endl<<endl;
+            cout<<major<<endl<<endl;
+            cout<<cgpa<<endl<<endl;
+            break;
+
+        }
+
+    }
+    file.close();
+    system("PAUSE");
+    showMenu();
 }
 void Student::mails(){
-
+    system("CLS");
+    ifstream file("announcementdata/student.txt");
+    while(getline(file,mailing)){
+        cout<<mailing<<endl<<endl;
+    }
+    file.close();
+    cout<<"Enter delete to delete or anything else to go back: ";
+    cin>>choice;
+    if(choice=="delete"){
+        ofstream file("announcementdata/student.txt");
+        file.close();
+        cout<<endl<<"Deleted"<<endl;
+        system("PAUSE");
+        showMenu();
+    }
+    else{
+        showMenu();
+    }
+}
+void Student::parentReg(){
+    system("CLS");
+    cout<<"Register parent username: ";
+    cin>>parentUser;
+    cout<<endl<<"Password: ";
+    cin>>parentPass;
+    ofstream file;
+    file.open("logindata/parents.txt",ios::app);
+    file<<parentUser<<" "<<parentPass<<endl;
+    file.close();
+    cout<<endl<<"Registered"<<endl;
+    system("PAUSE");
+    showMenu();
 }
--- portal/student.h
+++ portal/student.h
@@ -11,8 +11,18 @@ private:
     void menuWorker();
     void myDetail();
     void mails();
+    void parentReg();
     string userName;
     int opt;
+    string line;
+    string name;
+    string major;
+    string cgpa;
+    string mailing;
+    string choice;
+    string parentUser;
+    string parentPass;
+    string getName;
 };
 
 #endif // STUDENT_H
